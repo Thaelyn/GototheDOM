171d852e914e8a9697ddd1fde1cfa9ad6db5ea1a
Thaelyn <michael.c.legrand@gmail.com>
2017-07-06 10:23:31 +0200

    Corrections exercices

--- js/app.js
+++ js/app.js
@@ -1,11 +1,50 @@
 
-// console.log('hello world');  
+// console.log('hello world'); 
 
-var p = document.getElementById("addText");
-p.innerHTML = "Hello World";
+// Step 2 
 
-// var divParent = document.getElementById("addText");
-// var divEnfant = document.getElementById("remove");
+// Méthode 1
+var paragraph = document.getElementById("addText");
+paragraph.textContent = "Hello World";
 
-document.getElementById("addText").parentNode.removeChild(document.getElementById("remove"));
+// Méthode 2
+// addText.textContent = "Hello World";
 
+// Méthode 3
+var paragraph = document.getElementById("addText");
+paragraph.innerHTML = "Hello World"; 
+
+// Step 3
+
+// Méthode 1
+// var removeId = document.getElementById("remove");
+// removeId.parentNode.removeChild(removeId);
+
+// Méthode 2
+var removeId = document.getElementById("remove"); 
+removeId.remove();
+
+// Méthode 3
+// document.getElementById("addText").parentNode.removeChild(document.getElementById("remove"));
+
+
+// Step 4 
+
+// Méthode 1
+var changeColor = document.getElementById("changeColor");
+changeColor.style.backgroundColor = "Red";
+
+// Méthode 2 
+// var changeColor = document.getElementById("changeColor");
+// changeColor.classList.add("backgroundRed");
+
+// Step 5 
+
+// Méthode 1
+var addChildren = document.getElementById("addChildren");
+var child;
+
+for (var i = 0; i < 4; i++) {
+	child = document.createElement("p");
+	addChildren.appendChild(child);
+}
